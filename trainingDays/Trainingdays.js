1d42fddebc1f17245243c86ef49dd5e522f7bbce
Prity25-coder <pritynishad480@gmail.com>
2021-09-21 11:30:09 +0530

    adding training days in function

---
+++ trainingDays/Trainingdays.js
@@ -0,0 +1,60 @@
+// The scope of `random` is too loose 
+
+const name = 'Prashant';
+
+const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
+
+// The scope of `days` is too tight 
+const getTrainingDays = event => {
+  let days = 0;
+  if (event === 'Marathon') {
+    days = 50;
+  } else if (event === 'Triathlon') {
+    days = 100;
+  } else if (event === 'Pentathlon') {
+    days = 200;
+  } else {
+
+  }
+  return days;
+};
+
+// The scope of `name` is too tight 
+const logEvent = (name, event) => {
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+
+
+logEvent(name, event);
+logTime(name, days);
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+ 
+logEvent(name2, event2);
+logTime(name2, days2);
+
+const event3 = getRandEvent();
+const days3 = getTrainingDays(event3);
+const name3 = 'Prity';
+ 
+logEvent(name3, event3);
+logTime(name3, days3);
