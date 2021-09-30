de7d95d3f39ecd61bf27d901aed4e34dd6e4b683
Prity25-coder <pritynishad480@gmail.com>
2021-09-30 12:43:16 +0530

    adding toemoticon

---
+++ toEmoticon/toEmoticon.js
@@ -0,0 +1,34 @@
+// Write your function here:
+const toEmoticon = (emotion) =>{
+    switch(emotion){
+      case 'shrug':
+        return '|_{"}_|';
+      case 'smiley face' :
+        return ':)' ;
+      case 'frowny face':
+        return ':(';
+      case 'winky face':
+        return ';)';
+      case 'heart':
+        return '<3';
+      default:
+        return '|_(* ~ *)_|';        
+    }
+  
+  }
+  
+  console.log(toEmoticon('smiley face'));
+  console.log(toEmoticon('shrug'));
+  console.log(toEmoticon('frowny face'));
+  console.log(toEmoticon('winky face'));
+  console.log(toEmoticon('cute face'));
+  console.log(toEmoticon('heart'));
+  
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+  // console.log(toEmoticon("whatever")) 
+  // Should print  '|_(* ~ *)_|'
+  
+  // We encourage you to add more function calls of your own to test your code!
+  
